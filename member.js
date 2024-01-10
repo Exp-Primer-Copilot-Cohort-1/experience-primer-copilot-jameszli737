b1a83a66af02197d38f5f1363ce4b3f3fd47fb70
jameszli <112044971+jameszli737@users.noreply.github.com>
2024-01-10 23:05:12 +0000

    help me

--- member.js
+++ member.js
@@ -1,18 +1,9 @@
 function skillsMember() {
     var member = document.getElementById("member");
-    var skills = document.getElementById("skills");
-    var projects = document.getElementById("projects");
-    var contact = document.getElementById("contact");
-    var memberList = document.getElementById("memberList");
-    var skillsList = document.getElementById("skillsList");
-    var projectsList = document.getElementById("projectsList");
-    var contactList = document.getElementById("contactList");
-    member.style.backgroundColor = "#f5f5f5";
-    skills.style.backgroundColor = "#ffffff";
-    projects.style.backgroundColor = "#ffffff";
-    contact.style.backgroundColor = "#ffffff";
-    memberList.style.display = "block";
-    skillsList.style.display = "none";
-    projectsList.style.display = "none";
-    contactList.style.display = "none";
+    var memberSkills = document.getElementById("member-skills");
+    var memberSkillsButton = document.getElementById("member-skills-button");
+
+    memberSkillsButton.addEventListener("click", function() {
+        memberSkills.classList.toggle("hidden");
+    });
 }
